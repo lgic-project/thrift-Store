d6d48735d04b25966144c9917106813b3a05b1e0
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-04-25 07:49:44 +0545

    Add authentication module and related files in server/src/auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AtGuard, UserCheckGuard } from './common/guards';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     PrismaModule,
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: UserCheckGuard,
+    },
+  ],
 })
 export class AppModule {}
