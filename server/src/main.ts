87b831a8d5e4815ed025a195e120c2c11c83928c
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-05-24 13:28:47 +0545

    chore: Update server dependencies and add authentication module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Thrift-It Api')
+    .setDescription('The Thrift-It api description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(8000);
 }
 bootstrap();
