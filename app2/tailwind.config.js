5448a7d62382053953999245e05002a599a4a9c0
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-06-03 09:07:24 +0545

    feat: Add font family configuration to tailwind.config.js

--- app2/tailwind.config.js
+++ app2/tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pThin: ["Poppins-Thin", "sans-serif"],
+        pExtraLight: ["Poppins-ExtraLight", "sans-serif"],
+        pLight: ["Poppins-Light", "sans-serif"],
+        pRegular: ["Poppins-Regular", "sans-serif"],
+        pMedium: ["Poppins-Medium", "sans-serif"],
+        pSemibold: ["Poppins-SemiBold", "sans-serif"],
+        pBold: ["Poppins-Bold", "sans-serif"],
+        pExtrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pBlack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
