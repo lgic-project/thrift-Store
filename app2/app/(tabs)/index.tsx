e884af40ca1907860adef327a6c8cabddd19e4a3
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-06-06 02:43:43 +0545

    refactor: Update HomeScreen component in app2

--- app2/app/(tabs)/index.tsx
+++ app2/app/(tabs)/index.tsx
@@ -1,70 +1,79 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import PlayVideoList from "@/components/PlayVideoList";
+import { Colors } from "@/constants/Colors";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, useColorScheme } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const ForYouScreen = ({ activeTab }: { activeTab: string }) => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{ flex: 1 }}>
+      <PlayVideoList activeTab={activeTab} screenTab="forYou" />
+    </View>
+  );
+};
+
+const FollowingScreen = ({ activeTab }: { activeTab: string }) => {
+  return (
+    <View style={{ flex: 1 }}>
+      <PlayVideoList activeTab={activeTab} screenTab="following" />
+    </View>
+  );
+};
+
+export default function Home() {
+  const [index, setIndex] = useState(1); // Set initial index to 1 for "For You"
+  const [activeTab, setActiveTab] = useState<string>("forYou");
+
+  const handleIndexChange = (index: number) => {
+    setIndex(index);
+    setActiveTab(index === 0 ? "following" : "forYou");
+  };
+
+  const renderScene = ({ route }: { route: any }) => {
+    switch (route.key) {
+      case "forYou":
+        return <ForYouScreen activeTab={activeTab} />;
+      case "following":
+        return <FollowingScreen activeTab={activeTab} />;
+      default:
+        return null;
+    }
+  };
+  const colorScheme = useColorScheme();
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <TabView
+        navigationState={{
+          index,
+          routes: [
+            { key: "following", title: "Following" },
+            { key: "forYou", title: "For You" },
+          ],
+        }}
+        renderScene={renderScene}
+        onIndexChange={handleIndexChange}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            labelStyle={{
+              color: `${colorScheme === "dark" ? "rgb(229, 229, 231)" : "rgb(28, 28, 30)"}`,
+            }}
+            activeColor={`${
+              colorScheme === "dark" ? "white" : Colors.light.tint
+            }`}
+            indicatorStyle={{
+              backgroundColor: `${
+                colorScheme === "dark" ? "white" : "#0a7ea4"
+              }`,
+            }}
+            style={{
+              height: 50,
+              backgroundColor: "transparent",
+            }}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
