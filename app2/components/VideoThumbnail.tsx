b83c3f955ced6dfd2145cd750c4dd6acd6043b1e
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-06-08 19:50:22 +0545

    refactor: Update VideoThumbnail component in app2

--- app2/components/VideoThumbnail.tsx
+++ app2/components/VideoThumbnail.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { TabBarIcon } from "./navigation/TabBarIcon";
 import { router, useNavigation } from "expo-router";
 import { NavigationProp } from "@react-navigation/native";
@@ -16,12 +16,20 @@ interface VideoData {
 
 interface VideoThumbnailProps {
   video: VideoData;
+  hideViews?: boolean;
+  containerStyles?: string;
+  imageStyles?: string;
 }
 
-const VideoThumbnail = ({ video }: VideoThumbnailProps) => {
+const VideoThumbnail = ({
+  video,
+  hideViews,
+  containerStyles,
+  imageStyles,
+}: VideoThumbnailProps) => {
   const navigation = useNavigation<NavigationProp<any>>();
   return (
-    <View className="flex-1 m-1">
+    <View className={`flex-1 m-1 ${containerStyles}`}>
       <TouchableOpacity
         onPress={() => {
           navigation.navigate("[selectedVideo]", {
@@ -30,13 +38,16 @@ const VideoThumbnail = ({ video }: VideoThumbnailProps) => {
           });
         }}
       >
-        <View className="absolute z-10 bottom-1 left-1 flex-row items-center justify-center">
-          <TabBarIcon name="eye-outline" color={"white"} size={20} />
-          <Text className="text-white ml-1 mt-1 font-pRegular">11.2k</Text>
-        </View>
+        {!hideViews && (
+          <View className="absolute z-10 bottom-1 left-1 flex-row items-center justify-center">
+            <TabBarIcon name="eye-outline" color={"white"} size={20} />
+            <Text className="text-white ml-1 mt-1 font-pRegular">11.2k</Text>
+          </View>
+        )}
+
         <Image
           source={{ uri: video.thumbnail }}
-          className="w-full h-[200px] rounded-md"
+          className={`w-full h-[200px] rounded-md ${imageStyles}`}
         />
       </TouchableOpacity>
     </View>
