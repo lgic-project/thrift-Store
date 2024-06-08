73514e95a2ddfa94a13a8f8ed26f47f5c70af009
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-06-08 21:56:50 +0545

    refactor: Improve SearchInput component in app2

--- app2/components/SearchInput.tsx
+++ app2/components/SearchInput.tsx
@@ -2,29 +2,48 @@ import {
   View,
   Text,
   TextInput,
-  Image,
   TouchableOpacity,
   Alert,
 } from "react-native";
 import React, { useState } from "react";
-
-import { router, usePathname } from "expo-router";
-import { icons } from "@/constants/Icons";
+import { usePathname, useRouter } from "expo-router";
 import { TabBarIcon } from "./navigation/TabBarIcon";
 
 interface SearchInputProps {
   placeholder: string;
   initialQuery?: string;
   additionStyle?: string;
+  onSearch: (query: string) => void;
 }
 
 const SearchInput = ({
   placeholder,
   initialQuery,
   additionStyle,
+  onSearch,
 }: SearchInputProps) => {
   const pathname = usePathname();
   const [query, setQuery] = useState(initialQuery || "");
+  const router = useRouter();
+
+  const handleSearch = () => {
+    if (!query) {
+      Alert.alert(
+        "Missing query",
+        "Please input something to search results across database"
+      );
+      return;
+    }
+    if (pathname.startsWith("/search")) {
+      // Update the search query without navigating to a new page
+      onSearch(query);
+      router.setParams({ query });
+    } else {
+      onSearch(query);
+      router.push(`/search/${query}`);
+    }
+  };
+
   return (
     <View
       className={`bg-[#fff] w-full h-10 px-4 rounded-3xl items-center flex-row space-x-4 ${additionStyle}`}
@@ -35,21 +54,11 @@ const SearchInput = ({
         placeholder={placeholder}
         placeholderTextColor="#CDCDE0"
         onChangeText={(e) => setQuery(e)}
+        onSubmitEditing={handleSearch}
+        returnKeyType="search"
       />
 
-      <TouchableOpacity
-        onPress={() => {
-          if (!query) {
-            Alert.alert(
-              "Missing query",
-              "Please input something to search results across database"
-            );
-            return;
-          }
-          if (pathname.startsWith("/search")) router.setParams({ query });
-          else router.push(`/search/${query}`);
-        }}
-      >
+      <TouchableOpacity onPress={handleSearch}>
         <TabBarIcon name="search" color="grey" size={24} />
       </TouchableOpacity>
     </View>
