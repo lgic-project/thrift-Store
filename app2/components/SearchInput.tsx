a2b345ca3dbe7ced2d67ecc119acb52e42139bd8
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-06-08 18:34:26 +0545

    feat: Add SearchInput component to app2

---
+++ app2/components/SearchInput.tsx
@@ -0,0 +1,51 @@
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+
+import { router, usePathname } from "expo-router";
+import { icons } from "@/constants/Icons";
+import { TabBarIcon } from "./navigation/TabBarIcon";
+
+interface SearchInputProps {
+  placeholder: string;
+  initialQuery?: string;
+}
+
+const SearchInput = ({ placeholder, initialQuery }: SearchInputProps) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
+  return (
+    <View className=" bg-[#fff] w-full h-10 px-4 rounded-3xl items-center flex-row space-x-4">
+      <TextInput
+        className="text-base dark:text-white mt-0.5 flex-1 font-pregular"
+        value={query}
+        placeholder={placeholder}
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
+
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            Alert.alert(
+              "Missing query",
+              "Please input something to search results across database"
+            );
+          }
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
+        <TabBarIcon name="search" color="grey" size={24} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
