7c5248aca73e575e1c214d602e85a303474da810
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-06-09 16:09:24 +0545

    refactor: Update Discover screen in app2 to include price for videos

--- app2/app/(tabs)/discover/index.tsx
+++ app2/app/(tabs)/discover/index.tsx
@@ -16,12 +16,13 @@ interface VideoData {
   description: string;
   thumbnail: string;
   category: string;
+  price: number;
 }
 
 const videos: VideoData[] = [
   {
     id: 1,
-    title: "Product Title 1",
+    title: "Alovera Juice",
     username: "Joe Doe",
     url: "https://videos.pexels.com/video-files/15465878/15465878-sd_540_960_30fps.mp4",
     profilePic:
@@ -30,10 +31,11 @@ const videos: VideoData[] = [
     thumbnail:
       "https://images.pexels.com/photos/9136621/pexels-photo-9136621.jpeg?auto=compress&cs=tinysrgb&w=600",
     category: "Explore",
+    price: 198,
   },
   {
     id: 2,
-    title: "Product Title 2",
+    title: "Mango Juice",
     username: "Joe Doe",
     url: "https://videos.pexels.com/video-files/17687288/17687288-sd_540_960_30fps.mp4",
     profilePic:
@@ -42,10 +44,11 @@ const videos: VideoData[] = [
     thumbnail:
       "https://images.pexels.com/photos/15831205/pexels-photo-15831205/free-photo-of-american-flag-at-the-9-11-memorial-in-new-york-city-new-york.jpeg?auto=compress&cs=tinysrgb&w=600",
     category: "Popular",
+    price: 50,
   },
   {
     id: 3,
-    title: "Product Title 3",
+    title: "Nike Shoes",
     username: "Joe Doe",
     url: "https://videos.pexels.com/video-files/17169505/17169505-sd_540_960_30fps.mp4",
     profilePic:
@@ -54,10 +57,11 @@ const videos: VideoData[] = [
     thumbnail:
       "https://images.pexels.com/photos/18423763/pexels-photo-18423763/free-photo-of-handsome-gentleman-holding-crocodile-leather-iphone-15-case-by-gentcreate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     category: "Sports",
+    price: 100,
   },
   {
     id: 4,
-    title: "Product Title 4",
+    title: "Jordan Shoes",
     username: "Joe Doe",
     url: "https://videos.pexels.com/video-files/17687289/17687289-sd_540_960_30fps.mp4",
     profilePic:
@@ -66,10 +70,11 @@ const videos: VideoData[] = [
     thumbnail:
       "https://images.pexels.com/photos/19153800/pexels-photo-19153800/free-photo-of-fashion-man-couple-love.jpeg?auto=compress&cs=tinysrgb&w=600",
     category: "Music",
+    price: 129,
   },
   {
     id: 5,
-    title: "Product Title 5",
+    title: "Jordan Shirt",
     username: "Joe Doe",
     url: "https://videos.pexels.com/video-files/14907580/14907580-sd_540_960_30fps.mp4",
     profilePic:
@@ -78,10 +83,11 @@ const videos: VideoData[] = [
     thumbnail:
       "https://images.pexels.com/photos/15831205/pexels-photo-15831205/free-photo-of-american-flag-at-the-9-11-memorial-in-new-york-city-new-york.jpeg?auto=compress&cs=tinysrgb&w=600",
     category: "Anime",
+    price: 200,
   },
   {
     id: 6,
-    title: "Product Title 5",
+    title: "Katana Sword",
     username: "soul",
     url: "https://videos.pexels.com/video-files/14907580/14907580-sd_540_960_30fps.mp4",
     profilePic:
@@ -90,6 +96,7 @@ const videos: VideoData[] = [
     thumbnail:
       "https://images.pexels.com/photos/15831205/pexels-photo-15831205/free-photo-of-american-flag-at-the-9-11-memorial-in-new-york-city-new-york.jpeg?auto=compress&cs=tinysrgb&w=600",
     category: "Anime",
+    price: 136,
   },
   // Add more videos as needed
 ];
@@ -184,26 +191,36 @@ const Discover = () => {
       </View>
       <FlatList
         data={filledVideos}
-        numColumns={3}
+        numColumns={2}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item, index }) =>
           item.id !== -1 ? (
-            <VideoThumbnail
-              video={item}
-              key={index}
-              hideViews={true}
-              containerStyles="mt-6 mr-[6px] ml-[6px]"
-              imageStyles="h-[200px] rounded-3xl"
-            />
+            <View className="flex-1">
+              <VideoThumbnail
+                video={item}
+                key={index}
+                hideViews={true}
+                containerStyles="mt-6 mr-[6px] ml-[6px]"
+                imageStyles="h-[200px] rounded-3xl"
+              />
+              <View className="justify-center items-center">
+                <Text className="font-pBold pt-1 dark:text-white">
+                  {item.title}
+                </Text>
+                <Text className="font-pLight mt-[-5px] dark:text-white">
+                  {item.description}
+                </Text>
+                <Text className="font-pBold mt-[-5px] dark:text-white">
+                  ${item.price}
+                </Text>
+              </View>
+            </View>
           ) : (
             <View key={index} style={{ flex: 1, margin: 6, height: 200 }} />
           )
         }
         ListEmptyComponent={() => (
-          <EmptyState
-            title="No products found"
-            subTitle="No products found."
-          />
+          <EmptyState title="No products found" subTitle="No products found." />
         )}
       />
     </SafeAreaView>
@@ -211,9 +228,9 @@ const Discover = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      marginHorizontal: 10,
-    },
+  container: {
+    marginHorizontal: 10,
+  },
 });
 
 export default Discover;
