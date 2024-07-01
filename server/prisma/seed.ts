237ed1cec99012ad7f372bf7b6469db393ad5dc9
Soul-Xettri <rakeshchhetri420@gmail.com>
2024-07-01 09:06:46 +0545

    feat: Add websockets support for real-time communication

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,8 +1,36 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  const hash = await bcrypt.hash('password', 10)
+  await this.prisma.user.create({
+    data: {
+      email: 'superAdmin@gmail.com',
+      phone: '9812345678',
+      password: hash,
+      role: 'SUPERADMIN',
+      Profile: {
+        create: {
+          name: 'SuperAdmin',
+          username: '@super',
+          dob: '1950-02-23',
+          occupation: 'Administration',
+          voterId: '123456',
+          permanentAddress: 'Kathmandu',
+          temporaryAddress: 'Kathmandu',
+          referralCode: '9x9x9x',
+          province: 4,
+          district: 'kaski',
+          municipality: 'Pokhara',
+          ward: '26',
+        },
+      },
+    },
+  });
+  console.log('SuperAdmin Created...');
+}
 
 main()
   .then(async () => {
